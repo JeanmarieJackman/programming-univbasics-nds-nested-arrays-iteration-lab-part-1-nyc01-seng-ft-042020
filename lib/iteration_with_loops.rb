517d8f9cd6b1e85f90a53a98655c8e53c7be6341
Jeanmarie Jackman <jeanmarie.jackman@gmail.com>
2020-04-14 04:38:38 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,6 +7,13 @@ array_of_arrays = [
   [4,5,6],
   [7,8,9]
 ]
- 
+ row_index = 0
+while row_index < array_of_arrays.count do
+  element_index = 0
+  while element_index < array_of_arrays[row_index].count do
+    p array_of_arrays[row_index][element_index]
+    element_index += 1
+  end
+  row_index += 1
 find_even_values(array_of_arrays)
 end
